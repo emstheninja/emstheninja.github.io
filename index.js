8d33351ce0f999998c6aa2a0da7bd9933cefedd6
emstheninja <emstheninja@gmail.com>
2026-02-15 15:05:39 +1030

    cleanup and comment

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function generateSheet() {
   let sheetUrl = ""
   let sheetCode = "1Q7G1zLCa-K6Py4jfazyeFEiCKsGFSyD6Ln0znLvIjCY"
+  let sheetForm = "Overview!A1:F8"
+  let googleSheetsKey = "AIzaSyAo1G7vJX6pPOg7nV0poq7dyY4UX5m4o8M"
   let dataArray = []
   let dataArrayFormatted = ""
   document.getElementById("id01").innerHTML = ""
   var xmlhttp = new XMLHttpRequest();
   sheetUrl = document.getElementById("sheetUrl").value;
 
-  var url = "https://sheets.googleapis.com/v4/spreadsheets/1Q7G1zLCa-K6Py4jfazyeFEiCKsGFSyD6Ln0znLvIjCY/values/Overview!A1:F8?key=AIzaSyAo1G7vJX6pPOg7nV0poq7dyY4UX5m4o8M"
-  https://sheets.googleapis.com/v4/spreadsheets/1Q7G1zLCa-K6Py4jfazyeFEiCKsGFSyD6Ln0znLvIjCY/values/Sheet1!A1:D5
+  var url = "https://sheets.googleapis.com/v4/spreadsheets/" + sheetCode + "/values/" + sheetForm + "?key=" + googleSheetsKey
 
   xmlhttp.onreadystatechange = function () {
     setTimeout(function () {
@@ -19,18 +20,21 @@ function generateSheet() {
     if (this.readyState == 4 && this.status == 200) {
       var arr = JSON.parse(this.responseText)
       var tableBody = document.getElementById("wrap");
-
+      // Clear the table body before populating it with new data
       tableBody.innerHTML = "";
-      console.log(arr.values);
+
+      // Create the header row using the first row of the data
         const headerRow = document.createElement("tr");
         tableBody.appendChild(headerRow);
       for (const row of arr.values[0]) {
+
         const newCell = document.createElement("td");
+        // Set the text content of the cell to the value of the current row
+        // you can use newcell. something to set anything onto the element
         newCell.textContent = row;
         headerRow.appendChild(newCell);
       }
-
-
+      // Create the data rows using the remaining rows of the data
       for (const datum of arr.values.slice(1)) {
         const newRow = document.createElement("tr");
         tableBody.appendChild(newRow);
