951769f175c0eec6d68c32ff017a5771b58a8230
emstheninja <emstheninja@gmail.com>
2026-02-15 16:36:27 +1030

    remove overall duck

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function generateSheet() {
       track.innerHTML = '';
 
       // create one duck per user, positioned according to progress
-      users.forEach((u, i) => {
+      users.slice(0,-1).forEach((u, i) => {
         const pct = Math.min(100, Math.round((u.books / maxBooks) * 100));
         const item = document.createElement('div');
         item.className = 'duck-item';
